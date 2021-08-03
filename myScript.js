edcfecb83df5c612f40b4ec66b3d665d8c75a3ae
shrey1110 <31250007+shrey1110@users.noreply.github.com>
2021-08-03 11:14:21 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,11 +1,61 @@
 
   var coll = document.getElementsByClassName("collapsible");
   var i;
+  var w_menu = document.getElementById("Menu_DryFruits");
+  var x_menu = document.getElementById("Menu_Spices");
+  var y_menu = document.getElementById("Menu_Grocery");
+  var z_menu = document.getElementById("Menu_Seeds");
+  var w_product = document.getElementById("Product_DryFruits");
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+  w_menu.nextElementSibling.style.display="block";
+  w_product.style.display="block";
+
+  {
+    w_menu.addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
+      x_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="none";
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+
+    x_menu.addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      w_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="none";
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+
+    y_menu.addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      w_menu.nextElementSibling.style.display="none";
+      x_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="none";
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+
+    z_menu.addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      w_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="none";
+      x_menu.nextElementSibling.style.display="none";
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
@@ -13,6 +63,8 @@
       }
     });
   }
+
+
 
   function myFunction(string) {
     var w = document.getElementById("Product_DryFruits");
@@ -59,6 +111,61 @@
       } else {
         z.style.display = "block";
       }
+    }
+
+  }
+
+  function titleProduct(string) {
+    var w_menu = document.getElementById("Menu_DryFruits");
+    var x_menu = document.getElementById("Menu_Spices");
+    var y_menu = document.getElementById("Menu_Grocery");
+    var z_menu = document.getElementById("Menu_Seeds");
+    var w = document.getElementById("Product_DryFruits");
+    var x = document.getElementById("Product_Spices");
+    var y = document.getElementById("Product_Grocery");
+    var z = document.getElementById("Product_Seeds");
+    if(string==="Product_DryFruits"){
+      x_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="none";
+      w_menu.nextElementSibling.style.display="block";
+      x.style.display="none";
+      y.style.display="none";
+      z.style.display="none";
+      w.style.display = "block";
+
+
+    }
+    if(string==="Product_Spices"){
+      x_menu.nextElementSibling.style.display="block";
+      z_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="none";
+      w_menu.nextElementSibling.style.display="none";
+      w.style.display="none";
+      y.style.display="none";
+      z.style.display="none";
+      x.style.display = "block";
+
+    }
+    if(string==="Product_Grocery"){
+      x_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="none";
+      y_menu.nextElementSibling.style.display="block";
+      w_menu.nextElementSibling.style.display="none";
+      w.style.display="none";
+      x.style.display="none";
+      z.style.display="none";
+      y.style.display = "block";
+    }
+    if(string==="Product_Seeds"){
+      x_menu.nextElementSibling.style.display="none";
+      z_menu.nextElementSibling.style.display="block";
+      y_menu.nextElementSibling.style.display="none";
+      w_menu.nextElementSibling.style.display="none";
+      w.style.display="none";
+      x.style.display="none";
+      y.style.display="none";
+      z.style.display = "block";
     }
 
   }
